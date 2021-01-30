7d8dd0e05f86a9c5ecab135d1239056830bdd61e
Fabio La Farcioli <m3skalina@gmail.com>
2021-01-30 19:39:06 +0100

    First COmmit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '6.2.26',
+    'version' => '6.2.26.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'tecnickcom/tcpdf',
+  ),
+  'versions' => 
+  array (
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.2.26',
+      'version' => '6.2.26.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
